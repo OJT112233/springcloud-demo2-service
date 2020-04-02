3d4b04780ea28eaa9f96c07e1fa0d0b702bbfb00
oujianteng <oujianteng@ly-sky.com>
2020-04-02 14:30:44 +0800

    test注释

--- src/test/java/com/example/springbootservice/SpringbootServiceApplicationTests.java
+++ src/test/java/com/example/springbootservice/SpringbootServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.springbootservice;
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringbootServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.springbootservice;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringbootServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
